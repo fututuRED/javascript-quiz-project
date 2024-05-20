844d28d8d8a4aab6c57b2e2857378345c31e1f9b
Reda Dahmane <r.dahmanee@gmail.com>
2024-05-20 18:03:50 +0200

    average and one thing about the filter to do

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,76 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    this.currentQuestionIndex = 0;
+  }
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
+  }
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    if (answer === this.questions[this.currentQuestionIndex].answer)
+      console.log("In answer", answer);
+    console.log(this.questions);
+    {
+      this.correctAnswers++;
+    }
+  }
+  hasEnded() {
+    if (currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (currentQuestionIndex === this.questions.length) {
+      return true;
+    }
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if ((this.currentQuestionIndex = this.questions.length)) {
+      return true;
+    }
+  }
 
-    // 4. shuffleQuestions()
+  // 7. filterQuestionsByDifficulty() method
 
-    // 5. checkAnswer(answer)
+  filterQuestionsByDifficulty(difficulty) {
+    if (3 > difficulty > 1) {
+      return this.questions;
+    } else {
+      for (let question in this.questions) {
+        return this.questions.filter(
+          (difficul) => (this.questions.difficulty = difficul)
+        );
+      }
+    }
+  }
 
-    // 6. hasEnded()
+  /*
+
+*/
+  averageDifficulty() {
+    //array.reduce(accumulator, currentValue) => accumulator + current Value
+    // Sum of difficulties of each question / nb of questions
+  }
 }
